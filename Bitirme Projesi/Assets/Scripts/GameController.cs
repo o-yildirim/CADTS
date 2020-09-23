aec77a42b613be1bc0548496afc499e686a714a6
Bontu380 <onyldrm@hotmail.com>
2020-09-23 18:52:57 +0300

    Random string generation yapıldı

---
+++ Bitirme Projesi/Assets/Scripts/GameController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    private string withUppercase;
+    private string withoutUppercase;
+
+    public Text leftText;
+    public Text rightText;
+
+    void Start()
+    {
+        //Zaman tutulacak
+        newQuestion();
+    }
+
+    private void Update()
+    {
+        //Kullanıcıdan input alınacak
+    }
+
+    public void newQuestion()
+    {
+        int length = (int)Random.Range(1, 7);
+        withoutUppercase = StringManager.instance.generateString(length);
+        withUppercase = StringManager.instance.generateString(length);
+
+        withUppercase = StringManager.instance.assignRandomUppercase(withUppercase);
+
+        int randomTextIndex = (int)Random.Range(0, 2);
+
+        if (randomTextIndex == 0)
+        {
+            leftText.text = withUppercase;
+            rightText.text = withoutUppercase;
+            //O zaman doğru cevap sol olacak
+        }
+        else
+        {
+            rightText.text = withUppercase;
+            leftText.text = withoutUppercase;
+            //Doğru cevap sağ olacak
+        }
+
+
+    }
+
+}
+
---
+++ Bitirme Projesi/Assets/Scripts/StringManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StringManager : MonoBehaviour
+{
+    public static StringManager instance;
+    string x;
+
+    string[] characters = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    public string generateString(int length) //Length uzunluğunda rasgele string üretiyor. NOT: Karakterler tekrar edebiliyor.
+    {
+        string generated = "";
+
+        for (int i = 0; i < length; i++)
+        {
+            int randomIndex = (int)Random.Range(0, characters.Length);
+            generated = generated + characters[randomIndex];
+        }
+
+        return generated;
+    }
+
+    public string assignRandomUppercase(string stringToUse) //Stringin içindeki rasgele bir karakteri uppercase yapıyor.
+    {
+
+
+        int randomIndex = (int)Random.Range(0, stringToUse.Length);
+
+        char uppercasedLetter = char.ToUpper(stringToUse[randomIndex]);
+  
+        stringToUse = stringToUse.Substring(0, randomIndex) + uppercasedLetter + stringToUse.Substring(randomIndex+1, stringToUse.Length-randomIndex-1);
+
+        return stringToUse;
+
+    }
+
+}
