41823e0b3424274d3dac9dde763103ff2f2d5367
Bontu380 <onyldrm@hotmail.com>
2020-09-29 16:14:55 +0300

    Sceneler arası utiltiy manager taşınıyor

---
+++ Bitirme Projesi/Assets/Scripts/UtilityScripts/DontDestroy.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class DontDestroy : MonoBehaviour
+{
+    /*private static DontDestroy instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+
+    }*/
+    void Start()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag(this.gameObject.tag);
+
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+
+    }
+
+}
