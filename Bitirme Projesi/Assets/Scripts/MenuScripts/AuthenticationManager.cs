9f8d82e9ffb6bcbbec5789b46b17df996fa20382
Emre Kardaşlar <emre.kardaslar@gmail.com>
2021-04-24 15:53:53 +0300

    Register DateTime Genel Formata Oturtuldu

--- Bitirme Projesi/Assets/Scripts/MenuScripts/AuthenticationManager.cs
+++ Bitirme Projesi/Assets/Scripts/MenuScripts/AuthenticationManager.cs
@@ -5,6 +5,7 @@ using Google.Apis.Auth.OAuth2;
 using Newtonsoft.Json;
 using System;
 using Proyecto26;
+using System.Globalization;
 
 public class AuthenticationManager : MonoBehaviour
 {
@@ -115,18 +116,24 @@ public class AuthenticationManager : MonoBehaviour
             contactEmail = registerContactEmail.text;
 
         string unparsedDob = dateOfBirth.text;
-        DateTime date = DateTime.Parse(unparsedDob);
+        CultureInfo provider = CultureInfo.InvariantCulture;
+        DateTime temp;
+        var cultureInfo = new CultureInfo("de-DE");
+        if (DateTime.TryParse(dateOfBirth.text, cultureInfo, DateTimeStyles.NoCurrentDateDefault, out temp))
+        {
+            string parsedDob = temp.ToString("yyyy-M-dd");
+            string hashedPwd = GetMD5HashString(registerPassword.text);
 
-        string parsedDob = date.ToString("yyyy-M-dd");
 
-        string hashedPwd = GetMD5HashString(registerPassword.text);
-        
+            var registeredUser = new User(registerName.text, registerSurname.text, parsedDob, registerEmail.text, hashedPwd, contactEmail);
 
-        var registeredUser = new User(registerName.text,registerSurname.text,parsedDob,registerEmail.text, hashedPwd, contactEmail);
-
-        string email = encode(registerEmail.text); 
-        DatabaseHandler.registerUser(registeredUser, email, user => { });
-
+            string email = encode(registerEmail.text);
+            DatabaseHandler.registerUser(registeredUser, email, user => { });
+        }
+        else
+        {
+            status.text = "Lütfen doğum tarihinizi doğru formatta giriniz.";
+        }
     }
 
     public void login()
