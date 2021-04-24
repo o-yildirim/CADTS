360c9d2c0e0abb526959ea8541068c684e7f86b3
Bontu380 <onyldrm@hotmail.com>
2021-04-24 16:38:22 +0300

    Revert "Bükemediğim bilekte UI kitlerim"
    This reverts commit 4005f942e65d74ca1ebc9d27f5a411a64333a560.

--- Bitirme Projesi/Assets/Scripts/UtilityScripts/User.cs
+++ Bitirme Projesi/Assets/Scripts/UtilityScripts/User.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 
-/// <summary>
-/// The user class, which gets uploaded to the Firebase Database
-/// </summary>
-
 [Serializable] // This makes the class able to be serialized into a JSON
 public class User
 {
@@ -12,13 +8,25 @@ public class User
     public string dob;
     public string email;
     public string password;
+    public string contactMail;
 
-    public User(string name,string surname,string dob, string email, string password)
+    public User(string name, string surname, string dob, string email, string password)
     {
         this.name = name;
         this.surname = surname;
         this.dob = dob;
         this.email = email;
         this.password = password;
+        this.contactMail = "";
+    }
+
+    public User(string name, string surname, string dob, string email, string password, string contactMail)
+    {
+        this.name = name;
+        this.surname = surname;
+        this.dob = dob;
+        this.email = email;
+        this.password = password;
+        this.contactMail = contactMail;
     }
 }
